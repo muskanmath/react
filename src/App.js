61c24b28afa8071821de76dbf27cf79b49520b3b
muskanmath <muskanmathur750@gmail.com>
2024-07-13 13:17:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Abt from './components/Abt';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <Navbar title="Shop" aboutText="About Shop"/>
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyse"/>
+    <Abt/>
     </div>
+
+   </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+
+ //used state hook = create state variable
+ // text and settext were initialized using used state
+ // text = value
+ //settext = function to update settext
+
+export default function TextForm(props) {
+    const [text, setText] = useState(' ');
+
+
+    const handleUpClick = ()=>{
+    // console.log("uppercase was clicked" + text);
+      let newText = text.toUpperCase();
+      setText(newText)
+    }
+    
+    const handleLOClick = ()=>{
+      // console.log("uppercase was clicked" + text);
+      let newText = text.toLowerCase();
+      setText(newText)
+      }
+
+      const handleClearClick = ()=>{
+        // console.log("uppercase was clicked" + text);
+        let newText = '';
+        setText(newText)
+        }
+        
+        const handleCopy = ()=>{
+          var text = document.getElementById("myBox")
+          text.select();
+          navigator.clipboard.writeText(text.value);
+          }
+
+          const handleextraspaces = ()=>{
+            let newtext = text.split(/[ ]+/);
+            setText(newtext.join(" "))
+          }
+
+    const handleOnChange= (event)=>{ 
+    // console.log("onchange");
+      setText(event.target.value);
+    }
+    return (
+      <>
+
+      <div className="container">
+          <h1>{props.heading} </h1>
+        <div className="ab-3">
+          <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+        </div>
+          <button className="btn btn-primary mx-3 my-3" onClick={handleUpClick}>Convert to uppercase</button>
+          <button className="btn btn-primary" onClick={handleLOClick}>Convert to lowercase</button>
+          <button className="btn btn-primary mx-3" onClick={handleClearClick}>ClearText</button>
+          <button className="btn btn-primary mx-3" onClick={handleCopy}>copy text</button>
+          <button className="btn btn-primary mx-3" onClick={handleextraspaces}>handleextraspaces</button>
+      </div>
+
+      <div className="container my-3">
+        <h1> Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  )
+}
